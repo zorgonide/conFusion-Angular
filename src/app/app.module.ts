f10f6af883dc8383b552d05c6f74250241e195bb
zorgonide <ammar1998@gmail.com>
2021-02-25 19:35:49 +0530

    Assignment 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
 import { MatDialogModule } from '@angular/material/dialog';
-
+import { MatSliderModule } from '@angular/material/slider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -65,6 +65,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatCheckboxModule,
     FormsModule,
     MatSelectModule,
+    MatSliderModule,
     MatSlideToggleModule,
     ReactiveFormsModule,
   ],
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,31 +1,94 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../shared/comment';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
+  @ViewChild('fform') feedbackFormDirective;
 
   dish: Dish;
   dishIds: string[];
   prev: string;
   next: string;
+  commentForm: FormGroup;
+  comment: Comment;
 
+  formErrors = {
+    'author': '',
+    'comment': '',
+  };
+  validationMessages = {
+    'comment': {
+      'required':      'Comment is a required field.',
+      'minlength':     'Comment must be at least 1 characters long.'
+    },
+    'author': {
+      'required':      'Author name is a required field.',
+      'minlength':     'Author name must be at least 2 characters long.',
+      'maxlength':     'Author name cannot be more than 25 characters long.'
+    },
+  };
+  onValueChanged(data?: any) {
+    console.log(this.commentForm)
+    // from contact component
+    if (!this.commentForm) { return; }
+    const form = this.commentForm;
+    for (const field in this.formErrors ) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
-
+    private location: Location, 
+    private fb: FormBuilder) { this.createForm(); }
+  
+  createForm() {
+    this.commentForm = this.fb.group({
+      author: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      comment: ['', [Validators.required, Validators.minLength(1)] ],
+      rating: 5,
+    });
+    this.commentForm.valueChanges
+    .subscribe(data => this.onValueChanged(data));
+    this.onValueChanged();
+  }
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
     .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
   }
-
+  onSubmit() {
+    let com:Comment = this.commentForm.value;
+    com.date = new Date().toISOString();
+    this.dish.comments.push(com);
+    this.commentForm.reset({
+      author: '',
+      comment: '',
+      rating: 5
+    });
+    this.feedbackFormDirective.resetForm();
+  }
   setPrevNext(dishId: string) {
     const index = this.dishIds.indexOf(dishId);
     this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
